f8f9738f290504eb4fa9718bf87a232f017b6468
Leticia Bernardeli <134503671+leticiabernardeli@users.noreply.github.com>
2023-09-20 09:55:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_image');
+
+const form = document.querySelector('.form')
+const input = document.querySelector('.input_search')
+const buttonPrev = document.querySelector('.btn-prev')
+const buttonNext = document.querySelector('.btn-next')
+
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) =>{
+   const APIResponse = await fetch ('')
+(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+
+if (APIResponse.status === 200){
+  const data = await APIResponse.json();
+  return data;
+}
+}
+const renderPokemon = async (pokemon) =>{
+    pokemonName.innerHTML = 'Loading...'
+    pokemonNumber.innerHTML = '';
+   const data = await fetchPokemon(pokemon);
+
+ if(data){
+pokemonImage.style.display ='block';
+pokemonName.innerHTML=data.name;
+pokemonNumber.innerHTML=data.id;
+pokemonImage.src=data['sprites']['versions']['generations-v']
+['black-white']['animated']['front-default']
+input.value = '';
+searchPokemon = data.id;
+ }else{
+    pokemonImage.style.display = 'nome';
+    pokemonName.innerHTML = 'Not Found :c';
+    pokemonNumber.innerHTML = '';
+ }
+ } 
+ 
+
